0aec3f803681e9e88bc21ebe803589dbcea4f2ec
Juan Perez <paratrucos@gmail.com>
2016-10-30 02:27:23 -0600

    Primeros Cambios
    Cambios a titulos

--- agregarUsuarios.php
+++ agregarUsuarios.php
@@ -3,7 +3,7 @@
     <div class="col-lg-12">
         <div class="ibox float-e-margins">
             <div class="ibox-title">
-                <h5>Creacion de Usuarios con Git</h5>
+                <h5>Creacion de Usuarios</h5>
                 <div class="ibox-tools">
                     <a class="collapse-link">
                         <i class="fa fa-chevron-up"></i>
